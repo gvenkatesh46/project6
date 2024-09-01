1a7c4aeb73fb439a6d24658c6f3966ed711bce6a
gvenkatesh46 <godavarthivenkatesh46@gmail.com>
2024-09-01 23:47:01 +0530

    Add files via upload

---
+++ project4.js
@@ -0,0 +1,9 @@
+document.getElementById('donation-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission for demonstration purposes
+    alert('Thank you for your donation!');
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission for demonstration purposes
+    alert('Thank you for contacting us! We will get back to you soon.');
+});
